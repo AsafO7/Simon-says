5da7f376609d899989e40a756eeb6d486b7d6155
AsafO7 <ovasaf@gmail.com>
2022-09-21 14:13:00 +0300

    Version 1.0

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI).then(console.log(`MongoDB Connected`))
+    await mongoose.connect(process.env.MONGO_URI)
   } catch (error) {
     console.log(error)
     process.exit(1)
